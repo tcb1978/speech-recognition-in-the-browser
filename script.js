8d306e6c5b6b55909001d7ecaba10351c4144e7c
Matthew Eldredge <mattheweldredge@gmail.com>
2018-05-01 17:22:45 -0700

    get each transcribed word from the results array and join into a string

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ recognition.interimResults = true;
 
 //create a paragraph
 let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = [...e.results]
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+    console.log(transcript);
+})
+
+
+
+recognition.start();
